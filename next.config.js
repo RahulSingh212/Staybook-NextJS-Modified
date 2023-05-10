2d6a223d1fb3528792d3407c765b33a4674b2dc8
RahulSingh212 <rahulsinghrs174326@gmail.com>
2023-05-10 16:47:07 +0530

    profile-page-created

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ["icons8.com", "cdn.sanity.io", "lh3.googleusercontent.com"],
     disableStaticImages: true,

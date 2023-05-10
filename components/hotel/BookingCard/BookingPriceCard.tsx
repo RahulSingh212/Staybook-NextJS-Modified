fbde267a99ed72fca78073fdf3ef2b3f82e79062
RahulSingh212 <rahulsinghrs174326@gmail.com>
2023-05-10 17:09:38 +0530

    profile-page-created

--- components/hotel/BookingCard/BookingPriceCard.tsx
+++ components/hotel/BookingCard/BookingPriceCard.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { motion, motionValue } from "framer-motion";
-import Link from "next/link";
 import Image from "next/image";
-import Head from "next/head";
-import { useRouter } from "next/router";
 import { RoomDetails } from "@/classModels/bookings/roomDetails";
 import { BookingDetails } from "@/classModels/bookings/bookingDetails";
 

af0775157f2359bc5de495bd022ba039a10425e2
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2023-05-18 01:04:02 +0530

    do this shit

--- pages/api/inputlist.ts
+++ pages/api/inputlist.ts
@@ -67,6 +67,110 @@ async function handler(req: any, res: any) {
     },
   ];
 
+
+//   {
+//     "listings":{
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 25095,
+//             "name": "Staybook Atlantis Hotel",
+//             "address": "7977, Main Arakasha Pathway, Arya Nagar, Paharganj, New Delhi, Delhi 110055",
+//             "phone": "+918373929299",
+//             "latitude": "28.6445779",
+//             "longitude": "77.2081547",
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 23690,
+//             "name": "Staybook Hotel Jai Balaji New Delhi  Train Station",
+//             "address": "9918, Plot No. 9918, Street, Multani Dhanda, Paharganj, Delhi, 110055",
+//             "phone": 8373929299,
+//             "latitude": 28.6466052,
+//             "longitude": 74.2119263,
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 23540,
+//             "name": "Staybook Hotel Pinky Villa",
+//             "address": "XV-2463-N, Nalwa St, Chuna Mandi, Paharganj, New Delhi, Delhi 110055",
+//             "phone": 8373929299,
+//             "latitude": 28.6420735,
+//             "longitude": 77.2115974,
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 12347,
+//             "name": "Staybook Shivdev New Delhi Railway Station",
+//             "address": "851822 Arakashan Road, Behind Sheila Cinema, Arya Nagar, Paharganj, Delhi, 110055",
+//             "phone": 8373929299,
+//             "latitude": 28.6463784,
+//             "longitude": 77.2088524,
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 12348,
+//             "name": "Staybook WoodsView Mall Road Mussorie",
+//             "address": "Camels Back Rd, near Ambedkar Chowk Library, The Mall Road, Mussoorie, Uttarakhand 248179",
+//             "phone": 8373929299,
+//             "latitude": 30.4611914,
+//             "longitude": 77.9972672,
+//             "country": "IN"
+//         },
+//         "listing":{
+//             "id": 23719,
+//             "name": "Staybook@South Delhi",
+//             "address": "B-14, B Block, East of Kailash, New Delhi, Delhi 110065",
+//             "phone": 8373929299,
+//             "latitude": 28.5577775,
+//             "longitude": 77.2500378,
+//             "country": "IN"
+//         }
+//     }
+// }
   try {
     const xmlVal = 
     `

c6a5cd9e41cb6abdf6b9b67b49fcaf09f18260fe
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2023-05-23 02:08:43 +0530

    Revert "port in env"
    This reverts commit 2c89afda4bbc000ef61c94306e9ff66a7013e370.

---
+++ server.js
@@ -0,0 +1,26 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const HOSTNAME = 'localhost'
+const PORT = 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, hostname: HOSTNAME, port: PORT }) //
+const handle = app.getRequestHandler();
+
+
+const httpsOptions = {
+    key: fs.readFileSync("./https_cert/localhost-key.pem"),
+    cert: fs.readFileSync("./https_cert/localhost.pem")
+};
+
+app.prepare().then(() => {
+    createServer(httpsOptions, (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        handle(req, res, parsedUrl);
+    }).listen(PORT, (err) => {
+        if (err) throw err;
+        console.log("ready - started server on url: https://localhost:" + PORT);
+    });
+});
+
